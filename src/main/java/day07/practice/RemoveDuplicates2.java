9dc42dbf6506d5ba8a7c39d8159c0e405623e891
Chandru-Freshworks <118425963+Chandru-Freshworks@users.noreply.github.com>
2023-07-20 00:07:05 +0530

    code commit

--- src/main/java/day07/practice/RemoveDuplicates2.java
+++ src/main/java/day07/practice/RemoveDuplicates2.java
@@ -12,7 +12,7 @@ public class RemoveDuplicates2 {
         tasks.add(new Task("Task 1", LocalDate.of(2023, 7, 18))); 
         tasks.add(new Task("Task 3", LocalDate.of(2023, 7, 20)));
         tasks.add(new Task("Task 2", LocalDate.of(2023, 7, 19)));
-        tasks.add(new Task("Task 5", LocalDate.of(2023, 7, 20)));
+        tasks.add(new Task("Task 3", LocalDate.of(2023, 7, 20)));
         
         HashSet<Task> uniqueTasks = new HashSet<>(tasks);
         
